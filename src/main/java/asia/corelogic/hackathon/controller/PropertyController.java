c240e9cb4c7f7f367d5a9978dd895b6e99434538
Paul Carter <paulgcarter@optusnet.com.au>
2017-02-24 15:49:02 +1100

    Rental Avm

--- src/main/java/asia/corelogic/hackathon/controller/PropertyController.java
+++ src/main/java/asia/corelogic/hackathon/controller/PropertyController.java
@@ -3,6 +3,7 @@ package asia.corelogic.hackathon.controller;
 import asia.corelogic.hackathon.gateway.AvmGateway;
 import asia.corelogic.hackathon.gateway.PropertySuggestion;
 import asia.corelogic.hackathon.gateway.PropertySuggestionGateway;
+import asia.corelogic.hackathon.gateway.RentalAvmGateway;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,11 +16,13 @@ public class PropertyController {
 
     private PropertySuggestionGateway suggestionGateway;
     private AvmGateway avmGateway;
+    private RentalAvmGateway avmRental;
 
     @Autowired
-    public PropertyController(PropertySuggestionGateway suggestionGateway, AvmGateway avmGateway) {
+    public PropertyController(PropertySuggestionGateway suggestionGateway, AvmGateway avmGateway, RentalAvmGateway rentalAvmGateway) {
         this.suggestionGateway = suggestionGateway;
         this.avmGateway = avmGateway;
+        this.avmRental = rentalAvmGateway;
     }
 
     @GetMapping(path = "/suggestAddress")
@@ -28,7 +31,11 @@ public class PropertyController {
     }
 
     @GetMapping(path = "/avm")
-    public AvmGateway.FaresDetails getAddresses(@RequestParam int q) {
+    public AvmGateway.FaresDetails avm(@RequestParam int q) {
         return avmGateway.getAvm(q);
+    }
+    @GetMapping(path = "/avmRental")
+    public RentalAvmGateway.AvmResult avmRental(@RequestParam int q) {
+        return avmRental.getAvm(q);
     }
 }
---
+++ src/main/java/asia/corelogic/hackathon/gateway/RentalAvmGateway.java
@@ -0,0 +1,95 @@
+package asia.corelogic.hackathon.gateway;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+
+import static org.springframework.web.util.UriComponentsBuilder.fromHttpUrl;
+
+@Component
+public class RentalAvmGateway {
+    public static final String GATEWAY_PATH = "/v1/property/{propertyId}/rentalAvm.json";
+    private final RestTemplate restTemplate;
+    private final String path;
+//
+//    {
+//        "propertyId":0,
+//        "targetPropertyAvmInput":{
+//        "bathrooms":0,
+//            "bedrooms":0,
+//            "carSpaces":0,
+//            "craftsmanshipQuality":"MUCH_BETTER",
+//            "floorAreaM2":0,
+//            "landAreaM2":0,
+//            "propertyType":"COMMERCIAL",
+//            "saleDate":"string",
+//            "salePrice":0,
+//            "valuationDate":"string",
+//            "yearBuilt":0
+//    }
+//    }
+
+    @Autowired
+    public RentalAvmGateway(RestTemplate restTemplate,
+                            @Value("${corelogic.suggestion.uri}") String propertyUrl) {
+        this.restTemplate = restTemplate;
+        this.path = propertyUrl;
+    }
+
+    public AvmResult getAvm(int propertyId) {
+        String payload = String.format(
+            "{  \"propertyId\": %s}", propertyId);
+        JsonNode jsonNode = restTemplate.getForObject(getUrl(propertyId), JsonNode.class);
+        JsonNode details = jsonNode.path("rentalAvmDetail");
+        return new AvmResult(
+            details.get("rentalAvmEstimate").asInt(),
+            details.get("rentalAvmEstimateHigh").asInt(),
+            details.get("rentalAvmEstimateLow").asInt(),
+            details.get("rentalAvmPeriod").asText()
+        );
+    }
+
+    private URI getUrl(int propertyId) {
+        return fromHttpUrl(path)
+            .path(GATEWAY_PATH)
+            .buildAndExpand(propertyId)
+            .toUri();
+    }
+
+    public static class AvmResult {
+        private int rentalAvmEstimate;
+        private int rentalAvmEstimateHigh;
+        private int rentalAvmEstimateLow;
+        private String rentalAvmPeriod;
+
+        public AvmResult() {
+        }
+
+        public AvmResult(int rentalAvmEstimate, int rentalAvmEstimateHigh, int rentalAvmEstimateLow, String rentalAvmPeriod) {
+            this.rentalAvmEstimate = rentalAvmEstimate;
+            this.rentalAvmEstimateHigh = rentalAvmEstimateHigh;
+            this.rentalAvmEstimateLow = rentalAvmEstimateLow;
+            this.rentalAvmPeriod = rentalAvmPeriod;
+        }
+
+        public int getRentalAvmEstimate() {
+            return rentalAvmEstimate;
+        }
+
+        public int getRentalAvmEstimateHigh() {
+            return rentalAvmEstimateHigh;
+        }
+
+        public int getRentalAvmEstimateLow() {
+            return rentalAvmEstimateLow;
+        }
+
+        public String getRentalAvmPeriod() {
+            return rentalAvmPeriod;
+        }
+    }
+}
